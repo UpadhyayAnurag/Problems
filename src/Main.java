5fac1b7b5b79c7029491592d9bf0e89f9b2eb919
Anurag Upadhyay <aknurag75@gmail.com>
2025-06-25 21:30:15 +0530

    Corrected Structure for Project -> Added Main to git

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import Array.SecondMaximumInArray;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 30, 41, 5};
+        System.out.println(SecondMaximumInArray.secondMax(nums));
+    }
+}
