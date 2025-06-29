d927e6fcb15c738cc5cffcf2a4e74242ec573e32
Anurag Upadhyay <aknurag75@gmail.com>
2025-06-29 16:20:08 +0530

    Iterative way for rotation also added

--- src/Array/RotateArray.java
+++ src/Array/RotateArray.java
@@ -18,4 +18,28 @@ public class RotateArray {
             end--;
         }
     }
+
+
+    public static void rotate_Iterative(int[] nums, int k){
+        int size = nums.length;
+        k = k % size;
+        int[] aux = new int[k];
+
+
+        for (int i = 0; i < k; i++) {
+            aux[i] = nums[size - i - 1];
+        }
+
+        for (int i = size - 1; i > size - 1 - k; i--) {
+            nums[i] = nums[i - k];
+        }
+
+        for (int i = 0; i < aux.length; i++) {
+            nums[i] = aux[i];
+        }
+
+        for (int ele : nums) {
+            System.out.print(ele + " ");
+        }
+    }
 }
