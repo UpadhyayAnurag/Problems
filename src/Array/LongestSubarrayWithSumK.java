a5eb7f571cd0d540cd43094bf7c0b4f12d92f280
Anurag Upadhyay <aknurag75@gmail.com>
2025-07-11 23:39:48 +0530

    K sum Subarray soln

--- src/Array/LongestSubarrayWithSumK.java
+++ src/Array/LongestSubarrayWithSumK.java
@@ -1,4 +1,38 @@
 package Array;
 
+import java.util.HashMap;
+
 public class LongestSubarrayWithSumK {
+    public static int getLongestSubarray(int []a, long k) {
+        int n = a.length; // size of the array.
+
+        HashMap<Long, Integer> preSumMap = new HashMap<>();
+        long sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            //calculate the prefix sum till index i:
+            sum += a[i];
+
+            // if the sum = k, update the maxLen:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            // calculate the sum of remaining part i.e. x-k:
+            long rem = sum - k;
+
+            //Calculate the length and update maxLen:
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            //Finally, update the map checking the conditions:
+            if (!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
 }
