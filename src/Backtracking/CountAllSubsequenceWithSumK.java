da7dedb42f4734aa490feb94f1d530b695c1e063
Anurag Upadhyay <aknurag75@gmail.com>
2025-07-19 23:51:08 +0530

    Added Approach 2 for Subsequence sum equals K

--- src/Backtracking/CountAllSubsequenceWithSumK.java
+++ src/Backtracking/CountAllSubsequenceWithSumK.java
@@ -3,8 +3,8 @@ package Backtracking;
 public class CountAllSubsequenceWithSumK {
     static int count = 0;
     public static int totalCount(int[] nums, int k){
-        countSubsequences(nums, k, 0, 0);
-        return count;
+        //countSubsequences2(nums, k, 0, 0);
+        return countSubsequences2(nums, k, 0, 0);
     }
 
     private static void countSubsequences(int[] nums, int k, int idx, int sum){
@@ -18,5 +18,19 @@ public class CountAllSubsequenceWithSumK {
         countSubsequences(nums, k, idx+1, sum);
         sum -= nums[idx];
         countSubsequences(nums, k, idx+1, sum);
+    }
+
+
+    // 2nd Approach
+    private static int countSubsequences2(int[] nums, int k, int idx, int sum){
+        if(idx == nums.length){
+            if(sum == k){
+                return 1;
+            }
+            return 0;
+        }
+        int c1 = countSubsequences2(nums, k, idx+1, sum + nums[idx]);
+        int c2 = countSubsequences2(nums, k, idx+1, sum);
+        return c1+c2;
     }
 }
