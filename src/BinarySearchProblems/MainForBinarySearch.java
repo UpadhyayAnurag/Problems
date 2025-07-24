093d50d1e5456f06f46a543e173d4c68fa25809d
Anurag Upadhyay <aknurag75@gmail.com>
2025-07-24 23:32:31 +0530

    Solved Implement Lower Bound

--- src/BinarySearchProblems/MainForBinarySearch.java
+++ src/BinarySearchProblems/MainForBinarySearch.java
@@ -4,5 +4,8 @@ public class MainForBinarySearch {
     public static void main(String[] args) {
         // Recursive Binary Search
         System.out.println(RecursiveBinarySearch.binarySearch(new int[]{1,2,3,4,5,6,7,8,9,10}, 10));
+
+        // Implement Lower Bound
+        System.out.println(ImplementLowerBound.lowerBound(new int[]{1,2,3,4,5,6,7,8,9,10}, 101));
     }
 }
