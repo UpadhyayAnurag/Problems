5eda81110bb642e8527e0ffa833418ed16663dd3
Anurag Upadhyay <aknurag75@gmail.com>
2025-07-28 23:49:22 +0530

    Introduced First&LastOccurance class

---
+++ src/BinarySearchProblems/FirstAndLastOccurance.java
@@ -0,0 +1,7 @@
+package BinarySearchProblems;
+
+public class FirstAndLastOccurance {
+    public static int[] firstAndLastOccurance(int[] nums, int target){
+        return new int[]{-1,-1};
+    }
+}
