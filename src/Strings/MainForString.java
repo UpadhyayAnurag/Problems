9552b31de7b68a8222b87f1ef71f731784030710
Anurag Upadhyay <aknurag75@gmail.com>
2025-09-13 11:07:58 +0530

    Started with String Questions & Solved RemoveOuterParentheses

---
+++ src/Strings/MainForString.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class MainForString {
+    public static void main(String[] args) {
+        // Remove Outer Parentheses
+        System.out.println(RemoveOuterParentheses.removeOuterParentheses("()()(())((()))((())()())"));
+    }
+}
---
+++ src/Strings/README.md
@@ -0,0 +1,4 @@
+### Remove Outermost Parentheses from String
+- Problem Link: https://leetcode.com/problems/remove-outermost-parentheses/
+- Difficulty: Easy
+- Time Complexity: O(n)
